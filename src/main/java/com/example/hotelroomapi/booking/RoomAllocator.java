d7505486581f1b1a3bb337011a30b47ab872dd2f
Szymon Halastra <szymon.halastra@gmail.com>
2022-03-30 22:30:30 +0200

    Extract method and rename variables

--- src/main/java/com/example/hotelroomapi/booking/RoomAllocator.java
+++ src/main/java/com/example/hotelroomapi/booking/RoomAllocator.java
@@ -13,41 +13,46 @@ class RoomAllocator {
 
     private static final BigDecimal LIMIT = new BigDecimal("100");
 
-    private static boolean test(BigDecimal price) {
+    private static boolean divideBy(BigDecimal price) {
         return price.compareTo(LIMIT) >= 0;
     }
 
     public RoomAllocation allocate(final long premium, final long economy, final List<BigDecimal> bids) {
-        List<BigDecimal> sorted = bids
-                .stream()
-                .sorted(Comparator.reverseOrder()).toList();
+        var roomStandards = divideByPremiumAndEconomy(bids);
 
-        Map<Boolean, List<BigDecimal>> collect = sorted.stream()
-                .collect(Collectors
-                        .partitioningBy(RoomAllocator::test));
+        List<BigDecimal> premiumBids = roomStandards.get(Boolean.TRUE);
+        List<BigDecimal> economyBids = roomStandards.get(Boolean.FALSE);
 
-        long premiumBids = collect.get(Boolean.TRUE).size();
+        long premiumBidsAmount = premiumBids.size();
 
         BigDecimal premiumIncome, economyIncome;
         long reservedPremiumRooms, reservedEconomyRooms;
-        if (premiumBids >= premium) {
-            premiumIncome = collect.get(Boolean.TRUE).stream().limit(premium).reduce(BigDecimal.ZERO, BigDecimal::add);
+        if (premiumBidsAmount >= premium) {
+            premiumIncome = premiumBids.stream().limit(premium).reduce(BigDecimal.ZERO, BigDecimal::add);
             reservedPremiumRooms = premium;
 
-            List<BigDecimal> stepEconomy = collect.get(Boolean.FALSE).stream().limit(economy).toList();
+            List<BigDecimal> stepEconomy = economyBids.stream().limit(economy).toList();
             economyIncome = stepEconomy.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
             reservedEconomyRooms = stepEconomy.size();
         } else {
-            long missing = premium - premiumBids;
-            BigDecimal partial = collect.get(Boolean.TRUE).stream().reduce(BigDecimal.ZERO, BigDecimal::add);
-            premiumIncome = collect.get(Boolean.FALSE).stream().limit(missing).reduce(BigDecimal.ZERO, BigDecimal::add).add(partial);
-            reservedPremiumRooms = premiumBids + missing;
+            long missing = premium - premiumBidsAmount;
+            BigDecimal partial = premiumBids.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+            premiumIncome = economyBids.stream().limit(missing).reduce(BigDecimal.ZERO, BigDecimal::add).add(partial);
+            reservedPremiumRooms = premiumBidsAmount + missing;
 
-            List<BigDecimal> stepEconomy = collect.get(Boolean.FALSE).stream().skip(missing).limit(economy).toList();
+            List<BigDecimal> stepEconomy = economyBids.stream().skip(missing).limit(economy).toList();
             economyIncome = stepEconomy.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
             reservedEconomyRooms = stepEconomy.size();
         }
 
         return new RoomAllocation(reservedPremiumRooms, premiumIncome, reservedEconomyRooms, economyIncome);
+    }
+
+    private Map<Boolean, List<BigDecimal>> divideByPremiumAndEconomy(final List<BigDecimal> bids) {
+        return bids
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors
+                        .partitioningBy(RoomAllocator::divideBy));
     }
 }
