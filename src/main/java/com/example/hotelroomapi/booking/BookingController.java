904d5d89bd2cb3ef0b5089acb9ff3d113f5f4487
Szymon Halastra <szymon.halastra@gmail.com>
2022-04-03 13:19:00 +0200

    Add invalid body validation

--- src/main/java/com/example/hotelroomapi/booking/BookingController.java
+++ src/main/java/com/example/hotelroomapi/booking/BookingController.java
@@ -1,13 +1,17 @@
 package com.example.hotelroomapi.booking;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Validated
 @RestController
 public class BookingController {
 
@@ -21,6 +25,14 @@ public class BookingController {
     public RoomAllocation foo(@RequestParam("premium") long premiumRequested,
                               @RequestParam("economy") long economyRequested,
                               @RequestBody List<BigDecimal> bids) {
+        if (bids.isEmpty()) {
+            /*
+              Normally, I would use ControllerAdvice to handle exceptions,
+              the ResponseStatusException is used solely for the purpose of the task
+             */
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Bids list cannot be empty!");
+        }
+
         return roomAllocator.allocate(premiumRequested, economyRequested, bids);
     }
 }
